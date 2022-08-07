9a232df1d718bc68c114c765f134bbee951bd30b
satkur <pathfinder10.vox@gmail.com>
2022-08-07 16:09:00 +0900

    Allocation solved by binary search

--- Allocation.cpp
+++ Allocation.cpp
@@ -1,42 +1,52 @@
-#include <iostream>
-#include <vector>
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
 
-using namespace std;
+    using namespace std;
 
-int loadIntoTruck(int maxTruckNum, int capacity, vector<int>& loads) {
-    int truckNum = 1;
-    int loadCount = 0;
-    int weight = 0;
+    int loadIntoTruck(int maxTruckNum, int capacity, vector<int>& loads) {
+        int truckNum = 1;
+        int loadCount = 0;
+        int weight = 0;
 
-    for (size_t i = 0; i < loads.size(); i++) {
-        int weightSum = weight + loads[i];
+        for (size_t i = 0; i < loads.size(); i++) {
+            int weightSum = weight + loads[i];
 
-        if (capacity < weightSum) {
-            truckNum++;
-            if (maxTruckNum < truckNum) break;
-            weightSum = loads[i];
+            if (capacity < weightSum) {
+                truckNum++;
+                if (maxTruckNum < truckNum || capacity < loads[i]) break;
+                weightSum = loads[i];
+            }
+
+            weight = weightSum;
+            loadCount++;
+        }
+        return loadCount;
+    }
+
+    int main() {
+        int n = 0;
+        int k = 0;
+        cin >> n >> k;
+        
+        auto vecW = vector<int>(n);
+        
+        int w = 0;
+        int sumW = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> w;
+            vecW[i] = w;
+            sumW += w;
         }
 
-        weight = weightSum;
-        loadCount++;
-    }
-    return loadCount;
-}
+        int left = 0;
+        int right = sumW;
+        while(left < right) {
+            int mid = (left + right) / 2;
+            int q = loadIntoTruck(k, mid, vecW);
+            if (q >= n) right = mid;
+            else left = mid + 1;
+        }
 
-int main() {
-    int n = 0;
-    int k = 0;
-    cin >> n;
-    cin >> k;
-    
-    auto vecW = vector<int>(n);
-    
-    int w = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> w;
-        vecW[i] = w;
+        cout << right << endl;
     }
-
-    // wのmaxを取得し、nに合致するloadIntoTruckの戻り値を二分探索で求める
-    // nに合致する時の引数capacityが答え
-}
